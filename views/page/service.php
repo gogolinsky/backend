59edc2a76d404be46d1596afdbfe685e26669f3c
Павел Гоголинский <Gogolinsky.Pavel@gmail.com>
2020-02-07 15:55:46 +0300

    Add service link

--- views/page/service.php
+++ views/page/service.php
@@ -461,6 +461,7 @@ echo $this->render('/page/banners/banner', ['service' => $service]);
                                                                           <div class="accordion-item__body" v-if="opened">
                                                                               <div class="accordion-item__text text">
                                                                                   <p><?= $service['description'] ?></p>
+                                                                                  <a href="/page/service/<?= $service['alias'] ?>"></a>
                                                                               </div>
                                                                              <?php if(!empty($subServicesEquipments[$service['id']])): ?>
                                                                                  <div class="accordion-item__equipment">
